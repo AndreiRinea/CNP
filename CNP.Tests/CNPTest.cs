56e75406f483a945b1da8e43b47886152e9ec690
Andrei Rinea <arinea@intralinks.com>
2025-06-11 17:12:49 +0300

    initial commit

---
+++ CNP.Tests/CNPTest.cs
@@ -0,0 +1,21 @@
+﻿namespace CNP.Tests;
+
+public class CNPTest
+{
+    [Fact]
+    public void A_Valid_CNP_IsValidated()
+    {
+        var systemUnderTest = new CNP(1800101420010);
+        Assert.True(systemUnderTest.Validate());
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(180010142001)]
+    [InlineData(18001014200100)]
+    public void LongInt_Constructor_Rejects_OutOfRange_Values(long value)
+    {
+        Assert.Throws<ArgumentException>(() => new CNP(value));
+    }
+}
---
+++ CNP/CNP.cs
@@ -0,0 +1,91 @@
+﻿using System.Text;
+
+namespace CNP;
+
+public class CNP : IEquatable<CNP>
+{
+    private static readonly byte[] ValidationConstant = [2, 7, 9, 1, 4, 6, 3, 5, 8, 2, 7, 9];
+
+    private readonly byte[] _digits;
+
+    public CNP(long value)
+    {
+        if (value < 1000000000000 || value > 9999999999999)
+        {
+            throw new ArgumentException("invalid value " + value);
+        }
+        _digits = new byte[13];
+        int i = 12;
+        while (value > 0)
+        {
+            var digit = value % 10;
+            _digits[i] = (byte)digit;
+            value /= 10;
+            i--;
+        }
+    }
+
+    public CNP(byte[] digits)
+    {
+        if (digits == null) throw new ArgumentNullException();
+        if (digits.Length != 13) throw new ArgumentOutOfRangeException("value must be 13 digits");
+        for (int i = 0; i < 13; i++)
+        {
+            if (digits[i] < 0 || digits[i] > 9)
+                throw new ArgumentException("digit at index " + i + " has invalid value of " + digits[i]);
+        }
+        _digits = (byte[])digits.Clone();
+    }
+
+    public bool Validate()
+    {
+        int sum = 0;
+        for (int i = 0; i < 12; i++)
+        {
+            sum += _digits[i] * ValidationConstant[i];
+        }
+        var controlDigit = sum % 11;
+        return controlDigit == _digits[12];
+    }
+
+    public byte[] GetDigits()
+    {
+        return (byte[])_digits.Clone();
+    }
+
+    public override string ToString()
+    {
+        var result = new StringBuilder(13);
+        for (int i = 0; i < 13; i++)
+        {
+            result.Append(_digits[i]);
+        }
+        return result.ToString();
+    }
+
+    public override int GetHashCode()
+    {
+        var hashCode = new HashCode();
+        for (int i = 0; i < 13; i++)
+        {
+            hashCode.Add(_digits[i]);
+        }
+        return hashCode.ToHashCode();
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return Equals(obj as CNP);
+    }
+
+    public bool Equals(CNP? other)
+    {
+        if (ReferenceEquals(this, other)) return true;
+        if (other == null) return false;
+        return _digits.SequenceEqual(other._digits);
+    }
+
+    public static bool operator ==(CNP? left, CNP? right) => Equals(left, right);
+
+    public static bool operator !=(CNP? left, CNP? right) => !Equals(left, right);
+}
